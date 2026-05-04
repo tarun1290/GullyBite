5fef004197de1e95d64a54c237d3135afd603ac2
tarun1290 <tarun1290@gmail.com>
2026-05-04 10:21:04 +0530

    git commit -m "fix: update ecosystem.config.js for production environment"

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     cwd: __dirname,
     instances: 1,
     autorestart: true,
-    max_memory_restart: '250M',
+    max_memory_restart: '512M',
     env: {
       NODE_ENV: 'production',
       PORT: 3001,
